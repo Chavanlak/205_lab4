6c3673856ca69bb6a89174f3c58495788fdb6604
Chavanlak <chavanlak.p@ku.th>
2021-03-04 15:35:31 +0700

    if((n>0)&&(n<100))
    เงื่อนไข n

--- 6220503198_Four glass.c
+++ 6220503198_Four glass.c
@@ -2,8 +2,17 @@
 int main()
 {
     int a,b,c,d;
-    int n=0,z=0,m[100];
+    int n=0,sum=0,m[100];
     scanf("%d\n",&n);
+    if((n>0)&&(n<100))
+    {
+        for(a=0;a<n;a++)
+        {
+            gets(x);
+            int y[3]={1,2,3};
+            for()
+        }
+    }
 
 
 }
